4b35c76dddfc0b8804775d06164505f9380847bc
Zach Carroll <38516457+zcarroll4@users.noreply.github.com>
2022-02-24 14:46:47 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implemnts OnInit{
+    ngOnInit(): void {
   title = 'cashflow-dividends';
   window.scrollTo(0, 0);
+}
 }
