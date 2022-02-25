dee6c8ee2be218719d6062ea98c6cdef57d5ee1c
zcarroll4 <zachcarroll2015@gmail.com>
2022-02-25 00:09:53 -0500

    ff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,23 +8,7 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'cashflow-dividends';
-  constructor(private readonly titleService: Title, private readonly router: Router, private readonly activatedRoute: ActivatedRoute) { }
-  ngOnInit(): void {
-    this.router.events.pipe(filter((event: any) => event instanceof NavigationEnd), map(() => {
-      const pageTitle = this.titleService.getTitle();
-      const child = this.activatedRoute.firstChild;
-      if (child?.snapshot?.data['title']) {
-        return child?.snapshot?.data['title'];
-      }
-      return pageTitle;
-    })).subscribe({
-      next: tt => {
-        this.titleService.setTitle(tt),
-          window.scrollTo(0, 0);
-      }
-    });
-  }
-}
+
 }
