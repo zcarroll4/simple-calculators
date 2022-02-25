28a4ec80d710177023a1f64dc4bf9cb99a6e56dd
zcarroll4 <zachcarroll2015@gmail.com>
2022-02-25 00:07:56 -0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from "./home-page/home-page.component";
 import { RetirementIncomeComponent } from "./retirement-income/retirement-income.component";
 import { CoveredCallCalculatorComponent } from './covered-call-calculator/covered-call-calculator.component';
+import { MonthlyDividendCalculatorComponent } from './monthly-dividend-calculator/monthly-dividend-calculator.component';
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: 'Calculators/RetirementIncome', component: RetirementIncomeComponent },
-  { path: 'Calculators/CoveredCallETFIncome', component: CoveredCallCalculatorComponent },
-  { path: 'Calculators/MonthlyDivdendIncome', component: CoveredCallCalculatorComponent },
+  { path: 'Calculators/RetirementIncome', component: RetirementIncomeComponent, data: { title: 'Retirement Income' } },
+  { path: 'Calculators/CoveredCallETFIncome', component: CoveredCallCalculatorComponent, data: { title: 'Covered Call ETF Income Calculator' } },
+  { path: 'Calculators/MonthlyDivdendIncome', component: MonthlyDividendCalculatorComponent, data: { title: 'Monthly Dividend Income Calculator' } }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
