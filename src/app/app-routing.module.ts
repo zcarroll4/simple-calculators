530a1a0d36052ca67b92e86cc6913d72621fb146
zcarroll4 <zachcarroll2015@gmail.com>
2022-03-07 13:26:55 -0500

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomePageComponent } from "./home-page/home-page.component";
 import { RetirementIncomeComponent } from "./retirement-income/retirement-income.component";
 import { CoveredCallCalculatorComponent } from './covered-call-calculator/covered-call-calculator.component';
 import { MonthlyDividendCalculatorComponent } from './monthly-dividend-calculator/monthly-dividend-calculator.component';
+import { DividendCalendarComponent } from './dividend-calendar/dividend-calendar.component';
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'Calculators/RetirementIncome', component: RetirementIncomeComponent, data: { title: 'Retirement Income' } },
   { path: 'Calculators/CoveredCallETFIncome', component: CoveredCallCalculatorComponent, data: { title: 'Covered Call ETF Income Calculator' } },
-  { path: 'Calculators/MonthlyDivdendIncome', component: MonthlyDividendCalculatorComponent, data: { title: 'Monthly Dividend Income Calculator' } }
+  { path: 'Calculators/MonthlyDivdendIncome', component: MonthlyDividendCalculatorComponent, data: { title: 'Monthly Dividend Income Calculator' } },
+  { path: 'DividendCalendar', component: DividendCalendarComponent, data: { title: 'Dividend Calendar' } },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
