4f37abb2802829f5bf03870cf8f6201367fad895
zcarroll4 <zachcarroll2015@gmail.com>
2022-03-24 17:30:40 -0400

    go

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ export const routes: Routes = [
   { path: 'Calculators/CoveredCallETFIncome', component: CoveredCallCalculatorComponent, data: { title: 'Covered Call ETF Income Calculator' } },
   { path: 'Calculators/MonthlyDividendIncome', component: MonthlyDividendCalculatorComponent, data: { title: 'Monthly Dividend Income Calculator' } },
   { path: 'DividendCalendar', component: DividendCalendarComponent, data: { title: 'Dividend Calendar' } },
-  { path: 'i', component: VideoIntroComponent, data: { title: 'Welcome!' } },
+  { path: 'go', component: VideoIntroComponent, data: { title: 'Welcome!' } },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
